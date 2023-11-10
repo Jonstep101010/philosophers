148d19e46f04d958855e29c82fac6c207a8233ae
jschwabe <mail@jschwabe.site>
2023-11-10 09:34:16 +0000

    add indexing structure to table as philo_list

---
+++ test/philosim/test_philo.c
@@ -0,0 +1,21 @@
+#ifdef TEST
+
+#include "unity.h"
+
+#include "struct.h"
+#include "philo.h"
+
+void setUp(void)
+{
+}
+
+void tearDown(void)
+{
+}
+
+void test_philo_NeedToImplement(void)
+{
+    TEST_IGNORE_MESSAGE("Need to Implement philo");
+}
+
+#endif // TEST
--- test/utils/test_time.c
+++ test/utils/test_time.c
@@ -14,11 +14,12 @@ void tearDown(void)
 }
 
 #include <sys/time.h>
-void test_time_returnsval(void)
+void test_get_time_ms_returnsval(void)
 {
     // TEST_IGNORE_MESSAGE("Need to Implement time");
 	TEST_ASSERT_EQUAL_UINT64(time(NULL), get_time_ms()/1000);
 	TEST_ASSERT_UINT64_WITHIN(2, time(NULL), get_time_ms()/1000);
 }
+
 
 #endif // TEST
