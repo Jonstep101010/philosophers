de2665cccfdf5e06424714dc02579f8ce078a236
jschwabe <mail@jschwabe.site>
2023-11-19 17:43:17 +0000

    add files for strjoin with id of philo
    Signed-off-by: jschwabe <mail@jschwabe.site>

--- philo_bonus/src/utils_to.c
+++ philo_bonus/src/utils_to.c
@@ -6,10 +6,25 @@
 /*   By: jschwabe <jschwabe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/19 13:38:50 by jschwabe          #+#    #+#             */
-/*   Updated: 2023/11/19 13:42:55 by jschwabe         ###   ########.fr       */
+/*   Updated: 2023/11/19 17:25:10 by jschwabe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifdef TEST
+# include <stdlib.h>
+# include <string.h>
+void	*ft_calloc(size_t nitems, size_t size)
+{
+	void	*ptr;
+
+	if (nitems && (nitems * size) / nitems != size)
+		return (0);
+	ptr = malloc(nitems * size);
+	if (!ptr)
+		return (0);
+	return ((memset(ptr, 0, nitems * size)));
+}
+#endif
 #include "philo_bonus.h"
 
 static	int	get_digits(long n)
@@ -17,6 +32,8 @@ static	int	get_digits(long n)
 	int	digits;
 
 	digits = 0;
+	if (n <= 0)
+		digits++;
 	while (n)
 	{
 		n /= 10;
@@ -34,11 +51,45 @@ char	*ft_itoa(int n)
 	nbr = n;
 	len = get_digits(nbr);
 	s = (char *) ft_calloc(len + 1, sizeof(char));
+	if (!s)
+		return (0);
+	if (nbr < 0)
+	{
+		s[0] = '-';
+		nbr *= -1;
+	}
 	while (len)
 	{
-		s[len--] = (nbr % 10) + '0';
+		s[--len] = nbr % 10 + '0';
 		nbr /= 10;
 	}
 	return (s);
 }
 
+static	size_t	ft_strlen(const char *s)
+{
+	size_t	len;
+
+	len = 0;
+	while (*s++)
+		len++;
+	return (len);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*copy;
+	char	*p;
+
+	if (!s1 || !s2)
+		return (NULL);
+	copy = ft_calloc((ft_strlen(s1) + ft_strlen(s2) + 1), sizeof(char));
+	if (!copy)
+		return (NULL);
+	p = copy;
+	while (*s1)
+		*p++ = *s1++;
+	while (*s2)
+		*p++ = *s2++;
+	return (*p = '\0', copy);
+}
---
+++ philo_bonus/test/test_utils_to.c
@@ -0,0 +1,63 @@
+// #ifdef TEST
+
+#include "unity.h"
+
+#include "struct.h"
+#include "utils_to.c"
+#include <string.h>
+
+void setUp(void)
+{
+}
+
+void tearDown(void)
+{
+}
+
+void test_utils_to_itoa(void) {
+	for (int i = -1; ++i < 250;)
+		TEST_ASSERT_EQUAL_STRING(ft_itoa(i), ft_itoa(i));
+}
+
+void	test_strjoin_basic(void) {
+	char	*s1;
+	char	*s2;
+	char	*s3;
+
+	s1 = "Hello ";
+	s2 = "World!";
+	s3 = "Hello World!";
+	TEST_ASSERT_EQUAL_STRING(s3, ft_strjoin(s1, s2));
+}
+
+void	test_strjoin_empty(void) {
+	char	*s1;
+	char	*s2;
+	char	*s3;
+
+	s1 = "";
+	s2 = "";
+	s3 = "";
+	TEST_ASSERT_EQUAL_STRING(s3, ft_strjoin(s1, s2));
+}
+
+void	test_strjoin_null(void) {
+	TEST_ASSERT_NULL(ft_strjoin(NULL, "Hello"));
+	TEST_ASSERT_NULL(ft_strjoin(NULL, NULL));
+	TEST_ASSERT_NULL(ft_strjoin("World", NULL));
+}
+
+void	test_strjoin_number_id(void) {
+	for (int i = -1; ++i < 250;)
+		TEST_ASSERT_EQUAL_STRING(ft_itoa(i), ft_strjoin("", ft_itoa(i)));
+	char buffer[50] = "Hello, ";
+	strcat(buffer, "World!");
+	TEST_ASSERT_EQUAL_STRING("Hello, World!", buffer);
+	for (int i = -1; ++i < 250;) {
+		char buffer[10] = "/sem_";
+		strcat(buffer, ft_itoa(i));
+		TEST_ASSERT_EQUAL_STRING(buffer, ft_strjoin("/sem_", ft_itoa(i)));
+	}
+}
+
+// #endif // TEST
